607f72a87fbdfdf15d6de6d6454e428c3eaf2ef7
blacktunes <blacktunes@foxmail.com>
2021-01-11 16:46:24 +0800

    fix: 加载遗漏的懒加载插件

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from '@/assets/script/i18n'
 import router from '@/router'
+import lazyload from '@/assets/script/lazyload.js'
 import '@/assets/script/analytics/index'
 import '@/assets/script/voices'
 import '@/assets/style/transition.styl'
 import './registerServiceWorker.js'
 
-createApp(App).use(router).use(i18n).mount('#app')
+createApp(App).use(router).use(i18n).use(lazyload).mount('#app')
